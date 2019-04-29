83c40aa03257adb0ef152a0df24d60ce1531aa48
MallardDuck <dpock32509@gmail.com>
2019-04-29 13:55:05 -0700

    Add all the basic routes
    By using the route regex we will already be throwing HTTP not found exceptions if the input is larger than 4 characters long. Or excluded on accident and therefor would be null.

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,22 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return $router->app->version(); /// TODO: Add a real homepage.
+});
+
+$routeSize = "/{width:[0-9]{1,4}}/{height:[0-9]{1,4}}";
+$router->get($routeSize, function ($width, $height) use ($router) {
+    return "Regular Show Image"; /// TODO: Add a real homepage.
+});
+
+$router->get('/c' . $routeSize, function ($width, $height) use ($router) {
+    return "Crazy Show Image"; /// TODO: Add a real homepage.
+});
+
+$router->get('/g' . $routeSize, function ($width, $height) use ($router) {
+    return "Gray Show Image"; /// TODO: Add a real homepage.
+});
+
+$router->get('/gif' . $routeSize, function ($width, $height) use ($router) {
+    return "Gif Show Image"; /// TODO: Add a real homepage.
 });
