d1eba1fbd482ad31d3ff03733130cc021497cbc4
MallardDuck <dpock32509@gmail.com>
2019-04-29 14:02:24 -0700

    Adjust route regex & parameter bindings
    This will allow both the width and height to pass route verification as negative values. Again this is just to catch the error better and provide a more verbose/accurate error message. With this idea in mind, it will also accept 5 character sizes
    I’m also adding int as parameter bindings to ensure they are always a numeric value.

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,20 @@ $router->get('/', function () use ($router) {
 });
 
 $router->group(['middleware' => 'size'], function () use ($router) {
-  $routeSize = "/{width:[\-0-9]{1,4}}/{height:[0-9]{1,4}}";
-  $router->get($routeSize, function ($width, $height) use ($router) {
+  $routeSize = "/{width:[\-0-9]{1,5}}/{height:[\-0-9]{1,5}}";
+  $router->get($routeSize, function (int $width, int $height) use ($router) {
       return "Regular Show Image"; /// TODO: Add a real homepage.
   });
 
-  $router->get('/c' . $routeSize, function ($width, $height) use ($router) {
+  $router->get('/c' . $routeSize, function (int $width, int $height) use ($router) {
       return "Crazy Show Image"; /// TODO: Add a real homepage.
   });
 
-  $router->get('/g' . $routeSize, function ($width, $height) use ($router) {
+  $router->get('/g' . $routeSize, function (int $width, int $height) use ($router) {
       return "Gray Show Image"; /// TODO: Add a real homepage.
   });
 
-  $router->get('/gif' . $routeSize, function ($width, $height) use ($router) {
+  $router->get('/gif' . $routeSize, function (int $width, int $height) use ($router) {
       return "Gif Show Image"; /// TODO: Add a real homepage.
   });
 });
